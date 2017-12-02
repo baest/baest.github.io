feb967c17ed834244a02882484eda1b5440d8b09
Martin Kjeldsen <mk@bluepipe.dk>
2017-12-02 14:12:23 +0100

    Add onske gen

---
+++ onske_gen.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Template;
+use DateTime;
+use Data::Dumper;
+
+use vars qw/@argv/;
+
+my ($file, $year) = @ARGV;
+
+unless ($file) {
+	$year = DateTime->now->year;
+	$file = $year . '.txt';
+}
+
+die ("Please provide file") unless ($file && -r $file);
+
+if (!$year && $file =~ /(20\d{2})/) {
+	$year = $1;
+}
+
+die ("Please provide year") unless($year);
+
+open(FILE, $file) or die ($!);
+
+my %categories = ();
+my @wishes = ();
+my $last_category;
+
+while (<FILE>) {
+	chomp;
+	next if (/^\s*$/ || /^\*/);
+
+	unless (/^\s/) {
+		push_to_category($last_category, @wishes) if ($last_category);
+		@wishes = ();
+		$last_category = $_;
+		next;
+	}
+
+	s/^\s+//g;
+#	s!\s*//.*!!g;
+	s!(http(s)?://[\w_/\.-]+)!<a href="$1">$1</a>!g;
+
+	#[% x FILTER html %]
+
+	push @wishes, $_;
+}
+push_to_category($last_category, @wishes);
+
+close (FILE);
+
+sub push_to_category {
+	my $name = shift;
+	return unless(@_);
+	$categories{$name} = [sort {lc $a cmp lc $b} @_];
+}
+
+my $template = Template->new({
+	INTERPOLATE  => 1,
+#    DEBUG        => 'all',
+});
+
+my @categories = map {[$_, $categories{$_} ]} sort {lc $a cmp lc $b} keys %categories;
+
+
+$template->process(
+	'tt.htm.txt',
+	{
+		year => $year,
+#		data => \%categories,
+		data => \@categories,
+	}
+) || die $template->error(), "\n";
+
