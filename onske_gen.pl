9d17fe5ae9362ddaa4058c07b18e927aa94a950e
Martin Kjeldsen <mk@bluepipe.dk>
2017-12-10 20:24:50 +0100

    updated

--- onske_gen.pl
+++ onske_gen.pl
@@ -66,7 +66,7 @@ while (<FILE>) {
 
 	s/^\s+//g;
 #	s!\s*//.*!!g;
-	s!(http(s)?://[\wæøåÆØÅ_/\.\#\?-]+)!get_link($1)!eg;
+	s!(http(s)?://[\wæøåÆØÅ_/\.\#\?\!-]+)!get_link($1)!eg;
 
 	#[% x FILTER html %]
 
