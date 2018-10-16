91b553ed6bdff35257fcd77fad15841f803ba993
Martin Kjeldsen <mk@bluepipe.dk>
2018-10-16 20:15:42 +0200

    Run script on all files at once

--- onske_gen.pl
+++ onske_gen.pl
@@ -10,7 +10,7 @@ use Data::Dumper;
 use WWW::Shorten::TinyURL;
 #use WWW::Shorten::Bitly;
 use JSON::MaybeXS;
-use File::Slurp::Tiny qw/read_file write_file/;
+use File::Slurp::Tiny qw/read_file write_file read_lines/;
 use LWP::UserAgent;
 
 #https://www.engadget.com/2018/03/30/google-shutting-down-goo-gl-url-shortening-service/
@@ -26,6 +26,7 @@ my $json = JSON::MaybeXS->new(canonical => 1);
 my ($file, $template, $year) = @ARGV;
 my $name;
 my %url_map;
+my %categories = ();
 
 my $url_file = 'urls.txt';
 if (-f $url_file) {
@@ -38,7 +39,7 @@ unless ($file) {
 	$file = $year . '.txt';
 }
 
-die ("Please provide file") unless ($file && -r $file);
+#die ("Please provide file") unless ($file && -r $file);
 
 unless ($template) {
 	$template = 'tt.htm.txt';
@@ -52,37 +53,61 @@ if (!$year && $file =~ /(20\d{2})/) {
 
 die ("Please provide year") unless($year);
 
-open(FILE, $file) or die ($!);
-
-my %categories = ();
-my @wishes = ();
-my $last_category;
-
-while (<FILE>) {
-	chomp;
-	if (/^\*\s*name:\s+(\w+)/) {
-		$name = $1;
-	}
-	next if (/^\s*$/ || /^\*/);
-
-	unless (/^\s/) {
-		push_to_category($last_category, @wishes) if ($last_category);
-		@wishes = ();
-		$last_category = $_;
-		next;
-	}
-
-	s/^\s+//g;
-#	s!\s*//.*!!g;
-	s!(http(s)?://[\wæøåÆØÅ_/\.\#\?\!\=-]+)!get_link($1)!eg;
-
-	push @wishes, $_;
+for (qw/martin isabella gitte/) {
+	my $input_file = "$_\_2018.txt";
+	my $output_file = "gen/\u$_\_ønskeseddel.html";
+	next unless -r $input_file;
+	write_onske_file($input_file, $output_file);
 }
-push_to_category($last_category, @wishes);
 
 write_file($url_file, $json->encode(\%url_map));
 
-close (FILE);
+sub write_onske_file {
+	my ($file, $output_file) = @_;
+	%categories = ();
+
+	my @lines = read_lines($file, chomp => 1);
+	#open(FILE, $file) or die ($!);
+
+	my @wishes = ();
+	my $last_category;
+
+	for (@lines) {
+	#while (defined ($_ = <FILE>)) {
+		chomp;
+		if (/^\*\s*name:\s+(\w+)/) {
+			$name = $1;
+		}
+		next if (/^\s*$/ || /^\*/);
+
+		unless (/^\s/) {
+			push_to_category($last_category, @wishes) if ($last_category);
+			@wishes = ();
+			$last_category = $_;
+			next;
+		}
+
+		s/^\s+//g;
+	#	s!\s*//.*!!g;
+		s!(http(s)?://[\wæøåÆØÅ_/\.\#\?\!\=-]+)!get_link($1)!eg;
+
+		push @wishes, $_;
+	}
+
+	#close (FILE);
+
+	push_to_category($last_category, @wishes);
+
+	my @categories = map {[$_, $categories{$_} ]} sort {lc $a cmp lc $b} keys %categories;
+
+	my $output = Mojo::Template->new(vars => 1)->render_file($template, {
+			year => $year,
+			data => \@categories,
+			name => $name,
+	}) || die "$@\n";
+
+	write_file($output_file, $output);
+}
 
 sub get_link {
 	my ($url) = @_;
@@ -106,11 +131,3 @@ sub push_to_category {
 	return unless(@_);
 	$categories{$name} = [sort {lc $a cmp lc $b} @_];
 }
-
-my @categories = map {[$_, $categories{$_} ]} sort {lc $a cmp lc $b} keys %categories;
-
-say Mojo::Template->new(vars => 1)->render_file($template, {
-		year => $year,
-		data => \@categories,
-		name => $name,
-}) || die "$@\n";
